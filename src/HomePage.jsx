a3cf702a05056d253396014a66a8ee27280059ad
petsens <peeterr@tlu.ee>
2019-09-23 13:56:41 +0300

    Lesson3 valmis

---
+++ src/HomePage.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+import {products, washFluids} from "./mydatabase.js";
+import Header from "./Header.jsx";
+import ItemList from "./ItemList.jsx";
+
+class HomePage extends React.PureComponent{
+    constructor(props){
+        super(props);
+        this.state = {
+            items: products,
+        }
+    }
+
+    handleChange(event){
+        switch (event.target.value) {
+            case "products": {
+                this.setState({
+                    items: products,
+                });
+                break;
+            }
+            case "washFluids":{
+                this.setState({
+                    items: washFluids,
+                });
+                break;
+            }
+        }
+    };
+
+    render(){
+        return (
+            <>
+                <Header/>
+                <select onChange = {this.handleChange.bind(this)}>
+                    <option value = "products">Tooted</option>
+                    <option value = "washFluids">Aknapesuvedelikud</option>
+                </select>
+                <ItemList items = {this.state.items} />
+            </>
+        )
+    }
+}
+
+export default HomePage;
+
--- src/ItemPage.jsx
+++ src/ItemPage.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import Header from "./Header.jsx";
-import {products} from "./mydatabase";
+import {products, washFluids} from "./mydatabase";
 
 class ItemPage extends React.PureComponent{
     render(){
-        const item = phones[0];
+        const item = products[0];
         return (
             <>
             <Header/>
-            <div className = {"itemContainer"}>
+            <div className = {"item"}>
                 <img src = {item.imgSrc} />
                 <div className = {'item_title'}>{item.title}</div>
                 <div className = {'item_price'}>{item.price}</div>
