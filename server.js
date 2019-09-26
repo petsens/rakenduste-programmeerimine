a6ce611b73753091b739c1ca97f9dd3ffbdb86b7
petsens <peeterr@tlu.ee>
2019-09-26 14:01:57 +0300

    REST backend valmis, getItems ei tööta

--- server.js
+++ server.js
@@ -1,14 +1,29 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
 const path = require("path");
 const PORT = process.env.PORT || 3000;
+const DB = require("./server/database.js");
 
-app.get('/', (req, res) => {
+// GET all items
+app.get("/items", (req, res) =>{
+  res.json(DB.getItems);
+});
+
+//GET item with id
+app.get("/items/:itemId", (req, res) =>{
+  res.send(DB.getItem(req.params.itemId));
+});
+
+app.post("/hello", (req, res) =>{
+  res.send("post hello");
+});
+
+app.get("/", (req, res) => {
 	res.sendFile(path.resolve(__dirname, "dist", "index.html"));
-})
+});
 
-app.use(express.static('static'));
-app.use(express.static('dist'));
+app.use(express.static("static"));
+app.use(express.static("dist"));
 app.listen(PORT, () => {
   console.log("Server started", PORT);
   console.log(`http://localhost:${PORT}`);
