b341208701f42a6b0451a358564d7154eaf4b45e
petsens <peeterr@tlu.ee>
2019-10-13 11:59:46 +0300

    Mongo jamab

--- server/server.js
+++ server/server.js
@@ -3,6 +3,35 @@ const app = express();
 const path = require('path');
 const PORT = process.env.PORT || 3000;
 const DB = require("./database.js");
+const mongoose = require("mongoose");
+require('dotenv').config();
+
+var kittySchema = new mongoose.Schema({
+  name: String
+});
+
+var Kitten = mongoose.model('Kitten', kittySchema);
+
+const kitten1 = new Kitten({
+  name: "red cat"
+});
+
+const DB_URL = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASS}@cluster0-ru9x4.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+mongoose.connect(DB_URL)
+  .then(() => {
+    console.log("Database access success");
+    kitten1.save( err => {
+      if(err){
+        console.error("We had an error");
+      }
+      else{
+        console.log("Success, save!")
+      }
+    });
+  })
+  .catch( err => {
+    console.error("Error happened", err);
+  });
 
 // GET all items
 app.get("/api/items", (req, res) =>{
