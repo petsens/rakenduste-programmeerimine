5402364e38bd9ffb0be7b22457787fc1bf642f2e
petsens <peeterr@tlu.ee>
2019-10-21 13:32:09 +0300

    Otspunktid

--- server/server.js
+++ server/server.js
@@ -5,12 +5,17 @@ const PORT = process.env.PORT || 3000;
 const mongoose = require("mongoose");
 require('dotenv').config();
 const itemRouter = require("./item.router.js");
+const userRouter = require("./user.router.js");
 const DB = require("./database.js");
 const Item = require("./item.model.js");
+const bodyParser = require("body-parser");
 
 const DB_URL = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASS}@cluster0-ru9x4.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 
+app.use(bodyParser.json());
+
 app.use(itemRouter);
+app.use(userRouter);
 
 app.get('/', (req, res) => {
 	res.sendFile(path.resolve(__dirname, "../dist", "index.html"));
---
+++ server/user.router.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const router = express.Router();
+const User = require("./user.model.js");
+
+// Gets all users
+router.get("/api/users", (req, res) =>{
+    User.find({}, (err, docs) => {
+        if(err) return handleError(err, res);
+        res.status(200).json(docs);
+    });
+});
+
+// Login
+router.post("/api/users/login", (req, res) =>{
+    User.findOne({email: req.body.email}, (err, doc) =>{
+        if(err) return handleError(err, res);
+        res.send(doc);
+    })
+});
+
+// Creates new user(signup)
+router.post("/api/users", (req, res) =>{
+    const user = new User(req.body);
+    user.save((err) =>{
+        if(err) return handleError(err, res);
+        console.log("Success saved user");
+        res.status(200).json(user);
+    });
+});
+
+//Delete all users
+router.delete("/api/users/signup", (req, res) =>{
+    User.deleteMany({}, (err, docs) =>{
+        if(err) return handleError(err, res);
+        console.log(docs);
+        console.log("Success delete many users");
+        res.send(204);
+    });
+});
+
+function handleError(err, res){
+    console.log(err);
+    res.send(500);
+}
+
+module.exports = router;
