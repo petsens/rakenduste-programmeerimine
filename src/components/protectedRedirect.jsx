0a192258c785af3ef8081a1fc1c426cadc0c7a30
petsens <peeterr@tlu.ee>
2019-11-02 15:27:22 +0200

    Lesson8: HOC protected routes

---
+++ src/components/protectedRedirect.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import {Redirect} from "react-router-dom";
+import PropTypes from "prop-types";
+
+const protectedRedirect = (WrappedComponent) => {
+    return class extends React.PureComponent {
+        static displayName = "protected-redirect";
+        static propTypes = {
+            user: PropTypes.object.isRequired,
+        };
+        render () {
+            console.log("protected redirect", this.props);
+            if(!this.props.user.email) return <Redirect to = {"/"} />;
+            return (
+                <WrappedComponent {...this.props} />
+            );
+        }
+    };
+};
+
+export default protectedRedirect;
--- src/pages/UserPage.jsx
+++ src/pages/UserPage.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import PropTypes from "prop-types";
 import authConsumer from "../components/authConsumer.jsx";
+import protectedRedirect from "../components/protectedRedirect.jsx";
 
 class UserPage extends React.PureComponent {
 
@@ -17,4 +18,4 @@ class UserPage extends React.PureComponent {
     }
 }
 
-export default authConsumer(UserPage);
+export default authConsumer(protectedRedirect(UserPage));
